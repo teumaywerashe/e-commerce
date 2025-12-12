85e2197f8921db530957b70e8eecea58007346ad
teumaywerashe <teumay21@gmail.com>
2025-12-12 22:16:08 +0300

    fix: Update subproject commit to indicate dirty state

--- frontend/src/pages/OrderConformationPage.jsx
+++ frontend/src/pages/OrderConformationPage.jsx
@@ -30,33 +30,6 @@ const checkout = {
       price: 20,
       image: "https://picsum.photos/200?random=1",
     },
-    {
-      productID: 4,
-      name: "T-shirt",
-      size: "M",
-      color: "red",
-      quantity: 1,
-      price: 20,
-      image: "https://picsum.photos/200?random=1",
-    },
-    {
-      productID: 5,
-      name: "T-shirt",
-      size: "M",
-      color: "red",
-      quantity: 1,
-      price: 20,
-      image: "https://picsum.photos/200?random=1",
-    },
-    {
-      productID: 6,
-      name: "T-shirt",
-      size: "M",
-      color: "red",
-      quantity: 1,
-      price: 20,
-      image: "https://picsum.photos/200?random=1",
-    },
   ],
   shippingAdress: {
     address: "123 Fashion Street",
@@ -65,9 +38,11 @@ const checkout = {
   },
 };
 
-const calculateEstimatedDelivery=()=>{
-
-}
+const calculateEstimatedDelivery = (createdAt) => {
+  const orderDate = new Date(createdAt);
+  orderDate.setDate(orderDate.getDate() + 10);
+  return orderDate.toLocaleString();
+};
 
 function OrderConformationPage() {
   return (
@@ -77,7 +52,7 @@ function OrderConformationPage() {
       </h1>
       {checkout && (
         <div className="p-6 rounded-lg border">
-          <div className="flex justify-center mb-20">
+          <div className="flex justify-between mb-20">
             {/* order id and date*/}
             <div>
               <h2 className="text-xl font-semibold">
@@ -88,9 +63,34 @@ function OrderConformationPage() {
               </p>
             </div>
             {/* estimated delivery*/}
-            <div >
-              <p className="text-emerald-700 text-sm">Estimated Delivery: {calculateEstimatedDelivery(checkout.createdAt)}</p>
+            <div>
+              <p className="text-emerald-700 text-sm">
+                Estimated Delivery:{" "}
+                {calculateEstimatedDelivery(checkout.createdAt)}
+              </p>
             </div>
+          </div>
+          {/* ordered items */}
+          <div className="mb-4 p-4">
+            {checkout.checkOutItems.map((item) => (
+              <div key={item._id} className="flex items-center justify-between mb-4">
+                <div className="flex  gap-5 items-center justify-center">
+                  <img className="w-16 object-cover h-16" src={item.image} alt={item.name} />
+                  <div>
+                    <h1 className="text-lg font-bold text-black">
+                      {item.name}
+                    </h1>
+                    <p className="text-xl text-gray-400">
+                      {item.color}|{item.size}
+                    </p>
+                  </div>
+                </div>
+                <div>
+                  <p className="text-lg text-black">{item.price}</p>
+                  <p className="text-sm text-gray-400">City: {item.city}</p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       )}
